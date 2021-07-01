135cdee73ad6fe61961d47bc1b70150a2eef9ef4
TNVH1998 <86756164+TNVH1998@users.noreply.github.com>
2021-07-01 09:59:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
     introTitle: 'Đồ LÁO TOÉT à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    Mà sao Em cứ đi nhầm đường
+    Lạc vào tim Anh lẻ loi
+    Đằng sau chữ YÊU đây là THƯƠNG`,
     btnIntro: 'háhá',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    title: 'Phải chăng Em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng Em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích Anh <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Em lại yêu Anh <3',
+    btnReply: 'Gửi cho Anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    mess: 'Anh biết mà 🥰. Yêu Em nhiều nhiều 😘😘',
     messDesc: 'Hết dịch làm chuyến đi chơi nhờ, bé BAO anh nhóe :))',
     btnAccept: 'OK nháaaaaa <3',
     messLink: 'http://fb.com' https://m.me/hoanggg98}
